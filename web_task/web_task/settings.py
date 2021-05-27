f5234111f1beaa0ddbeac418bd45f48e52f47e80
Aleksandra Samol <44066048+salmonellix@users.noreply.github.com>
2021-05-27 17:45:32 +0200

    Update settings.py

--- web_task/web_task/settings.py
+++ web_task/web_task/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'addressbook',
         'USER': 'root',
-        'PASSWORD': 'xe587kop_',
+        'PASSWORD': '***********',
         'HOST': 'localhost',
         'PORT': '3306',
             }
